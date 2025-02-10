4743ddfe8f73f7a402a6c79a0903050708425a84
pseudoDjnn <lavahrexes@gmail.com>
2025-02-10 12:56:01 -0500

    add word_count function

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+def count_characters():
+
+def word_count(words):
+    result = words.split()
+    return len(result)
+
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+    # Now call word_count and print the result
+    # Can you add these lines?
+
+    result = word_count(file_contents)
+    print(result)
+
+main()
