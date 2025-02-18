48e6cc1077c0e530d38ea2e103e83a73a53c6713
pseudoDjnn <lavahrexes@gmail.com>
2025-02-18 01:56:20 -0500

    added argparse

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
+import argparse
+
 from word_counter import word_count
 from character_counter import count_characters
 from comp_report import comp_report
 
 
+def parser():
+    parse = argparse.ArgumentParser(description='A BookBot: A text analysis tool')
+    parse.add_argument('book', help='Path to the book file to analyze')
+    return parse.parse_args()
+
+
 
 def main():
-    with open("books/frankenstein.txt") as file:
+    
+    search = parser()
+    
+    with open(search.book) as file:
         file_contents = file.read()
     # Now call word_count and print the result
     # Can you add these lines?
 
     words = word_count(file_contents)
-    # print(words)
+    print(words)
 
     count = count_characters(file_contents)
     # print(count)
     
     report = comp_report(file_contents)
-    print(report)
+    # print(report)
 
 main()
