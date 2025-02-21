08f80149f99b2c7653c825635652a240c81d54c3
pseudoDjnn <lavahrexes@gmail.com>
2025-02-20 23:08:02 -0500

    basic idea for class is now setup

--- utils/cli_interface.py
+++ utils/cli_interface.py
@@ -1,17 +1,40 @@
-def welcome_display():
-  """
+class TextAnalyzerInterface:
+  def __init__(self):
+    # This will be the TextAnalyzer instance once we have text
+    self.analyzer = None
+    self.text = None
 
-  Initial Welcome Banner
-  
-  """
-  
-  print("\nWelcome to the Text Analyzer")
-  print("----------------------------")
-  
-  while True:
-    print("\nPlease select option:")
-    print("1.  Input text directly")
-    print("2.  Load text from file")
-    print("3.  Exit")
+    """
+
+    Initial Welcome Banner
     
-    choice = input("\nEnter choice (1-3): ")
+    """
+    
+  def welcome_display(self):
+    
+    print("\nWelcome to the Text Analyzer")
+    print("----------------------------")
+    
+    self.show_main_menu()
+    
+  def show_main_menu(self):
+    
+    while True:
+      print("\nPlease select option:")
+      print("1.  Input text directly")
+      print("2.  Load text from file")
+      print("3.  Exit")
+        
+      choice = input("\nEnter choice (1-3): ")
+      self.handle_choice(choice)
+        
+      """
+
+      Once we get valid text, we will create a TextAnayzer instance
+      self.analyzer = TextAnalyzer(self.text)
+      The we can call the existing analysis methods
+      
+      """
+      
+  def handle_choice(self, choice):
+    pass
