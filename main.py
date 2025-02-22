9bb0c98c065ca1568dbfa8ac6125ccad00388fae
pseudoDjnn <lavahrexes@gmail.com>
2025-02-21 23:46:12 -0500

    full rework now in progress

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 
-# main.py
 from analyzers import TextAnalyzer
-from utils import TextAnalyzerInterface
 
 def main():
-    interface = TextAnalyzerInterface()
     
     # Read Frankenstein and soon to be other .txt files
     with open('books/frankenstein.txt') as file:
