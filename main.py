1f9b26c702a19264b664f73f8aaa86534e326626
pseudoDjnn <lavahrexes@gmail.com>
2025-02-22 00:09:02 -0500

    Flask is up and running now

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+from flask import Flask, render_template, request, jsonify
 
 from analyzers import TextAnalyzer
 
-def main():
-    
-    # Read Frankenstein and soon to be other .txt files
-    with open('books/frankenstein.txt') as file:
-        file_contents = file.read()
-    
+app = Flask(__name__)
 
-main()
+@app.route('/')
+def index():
+    # Serve the html form
+    return render_template('index.html')  # HTML file in templates foler
+
+@app.route('/analyze/', methods=['POST'])
+def main():
+    # Get text from form submission
+    text = request.form.get("text", "")
+    
+    # Pass text to the TextAnalyzer
+    analyzer = TextAnalyzer(text)
+    
+    # Get analysis results
+    word_count = analyzer.get_word_count()
+    char_count = analyzer.get_char_count()
+    
+    # Return results as JSON
+    return jsonify({
+        'word_count': word_count,
+        'char_count': char_count
+    })
+    
+if __name__ == '__main__':
+    app.run(debug=True)
+    main()
