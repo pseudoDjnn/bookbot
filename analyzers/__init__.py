37109a85f927829ef704c87b579d9ee8b649abb0
pseudoDjnn <lavahrexes@gmail.com>
2025-02-23 00:43:56 -0500

    updated __init__.py file in analyzers folder for pathing purpose

--- analyzers/__init__.py
+++ analyzers/__init__.py
@@ -1,7 +1,7 @@
 # analyzers/__init__.py
-from .text_analyzer import TextAnalyzer
+from .text_analyzer import TextAnalyzer,WordCountAnalyzer, CharacterCountAnalyzer, CharacterFrequencyAnalyzer
 # from .total_word_counter import count_total_words
 # from .total_char_counter import count_total_chars
 # from .char_frequency_analyzer import analyze_char_frequency
 
-__all__ = ['TextAnalyzer']
+__all__ = ['TextAnalyzer', 'WordCountAnalyzer', 'CharacterCountAnalyzer', 'CharacterFrequencyAnalyzer']
