275ecb04c77a9a01c262192a844e002bfd21f03e
pseudoDjnn <lavahrexes@gmail.com>
2025-02-28 00:18:38 -0500

    added back the character count

--- main.py
+++ main.py
@@ -16,16 +16,25 @@ def analyze():
     analysis_type = request.form['analysis_type']
     
     word_count = None
+    char_count = None
     
     
     if analysis_type == 'word_count' or analysis_type == 'all':
         # Use the class instead of duplicating logic
         word_analyzer = WordCountAnalyzer(text)
         word_count = word_analyzer.analyze()
+        
+    if analysis_type == 'char_count' or analysis_type == 'all':
+        char_count_analyzer = CharacterCountAnalyzer(text)
+        char_count = char_count_analyzer.analyze()
+        
+        
     
     return render_template('results.html',
                          analysis_type=analysis_type,
-                         word_count=word_count)
+                         word_count=word_count,
+                         char_count=char_count
+                         )
 
 # Run the app in debug mode
 if __name__ == '__main__':
